efd5fe1d95520158a8339d056df989e53bdca6f5
Larhdid ilyas <larhdidok@gmail.com>
2024-01-22 15:52:39 +0000

    finished : google auth

--- src/main/java/com/larhdid/security/web/CustomerController.java
+++ src/main/java/com/larhdid/security/web/CustomerController.java
@@ -3,6 +3,7 @@ package com.larhdid.security.web;
 import com.larhdid.security.entities.Customer;
 import com.larhdid.security.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +13,29 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/customers")
 public class CustomerController {
     private final CustomerRepository customerRepository;
 
     @GetMapping
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/customers")
     public String customers(Model model){
         List<Customer> customers = customerRepository.findAll();
         model.addAttribute("customers",customers);
         return "customers";
+    }
+
+    @GetMapping("/products")
+    public String products(Model model){
+        List<Customer> customers = customerRepository.findAll();
+        model.addAttribute("customers",customers);
+        return "products";
+    }
+
+    @GetMapping("/auth")
+    public Authentication authentication(Authentication authentication){
+        return authentication;
     }
 }
